9da1b51fc5772aca6b883f74b3c0cef5e3d394b1
Tu Ha Dinh <hadinhtu97@gmail.com>
2021-02-28 09:14:32 +0700

    Refactor: make project only backend

--- server.js
+++ server.js
@@ -14,25 +14,17 @@ app.use(cors({ optionsSuccessStatus: 200 }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.static(__dirname + "/public"));
-
 app.route('/').get((req, res) => {
-    res.sendFile(__dirname + '/views/index.html');
+    res.send('Read README to use api.');
 });
 
-// Routes below here
-
-
 apiRoutes(app);
-
-
-// Routes above here 
 
 app.use((req, res) => {
     res.status(404);
     res.send('Not Found');
 })
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log('Your app is listening on port ' + process.env.PORT);
+app.listen(3000, () => {
+    console.log('Your app is listening on port 3000');
 })
